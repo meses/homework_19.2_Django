0bf6c2de83ff5987ef59f71224efd3891c9b5111
Artem <akirillov@itps-russia.ru>
2023-05-30 00:52:08 +0500

    Попытка исправления кодировки

--- catalog/management/commands/fill_product.py
+++ catalog/management/commands/fill_product.py
@@ -7,14 +7,25 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         products_lst = [
-            {'title': 'Ноутбук Huawei MateBook D 15 BOD-WDI9','description': '15.6", IPS, Intel Core i3 1115G4 3ГГц, 2-ядерный, 8ГБ DDR4, 256ГБ SSD, Intel UHD Graphics , без операционной системы, серый космос [53013sdv]','price': 42999, 'category': Category.objects.get(title='Ноутбуки')},
-            {'title': 'Ноутбук HIPER Workbook N15RP, 15.6','description': 'AMD Ryzen 5 3500U 2.1ГГц, 4-ядерный, 16ГБ DDR4, 512ГБ SSD, AMD Radeon Vega 8, Windows 10 Professional, черный [n15rp96wi]','price': 44990, 'category': Category.objects.get(title='Ноутбуки')},
-            {'title': 'Ноутбук игровой HIPER G16','description': '16.1", Intel Core i5 10400 2.9ГГц, 6-ядерный, 16ГБ DDR4, 512ГБ SSD, NVIDIA GeForce RTX 3070 для ноутбуков - 8 ГБ, без операционной системы, черный [g16rtx3070a10400lx]','price': 125990, 'category': Category.objects.get(title='Ноутбуки')},
-            {'title': 'Смартфон Huawei Mate 50 Pro 8/512Gb', 'description': 'оранжевый', 'price': 79990,'category': Category.objects.get(title='Смартфоны')},
-            {'title': 'Смартфон REALME C25s 4/64Gb', 'description': 'серый', 'price': 8990,'category': Category.objects.get(title='Смартфоны')},
-            {'title': '50" Телевизор HAIER Smart TV S1', 'description': '4K Ultra HD, черный, СМАРТ ТВ, Android','price': 27990, 'category': Category.objects.get(title='Телевизоры')},
-            {'title': '43" Телевизор Xiaomi Mi TV A2', 'description': '4K Ultra HD, черный, СМАРТ ТВ, Android','price': 24990, 'category': Category.objects.get(title='Телевизоры')},
-            {'title': 'Монитор Huawei MateView SE SSN-24 23.8"', 'description': 'черный [53060683]', 'price': 11990,'category': Category.objects.get(title='Мониторы')}
+            {'title': 'РќРѕСѓС‚Р±СѓРє Huawei MateBook D 15 BOD-WDI9',
+             'description': '15.6", IPS, Intel Core i3 1115G4 3Р“Р“С†, 2-СЏРґРµСЂРЅС‹Р№, 8Р“Р‘ DDR4, 256Р“Р‘ SSD, Intel UHD Graphics , Р±РµР· РѕРїРµСЂР°С†РёРѕРЅРЅРѕР№ СЃРёСЃС‚РµРјС‹, СЃРµСЂС‹Р№ РєРѕСЃРјРѕСЃ [53013sdv]',
+             'price': 42999, 'category': Category.objects.get(title='РќРѕСѓС‚Р±СѓРєРё')},
+            {'title': 'РќРѕСѓС‚Р±СѓРє HIPER Workbook N15RP, 15.6',
+             'description': 'AMD Ryzen 5 3500U 2.1Р“Р“С†, 4-СЏРґРµСЂРЅС‹Р№, 16Р“Р‘ DDR4, 512Р“Р‘ SSD, AMD Radeon Vega 8, Windows 10 Professional, С‡РµСЂРЅС‹Р№ [n15rp96wi]',
+             'price': 44990, 'category': Category.objects.get(title='РќРѕСѓС‚Р±СѓРєРё')},
+            {'title': 'РќРѕСѓС‚Р±СѓРє РёРіСЂРѕРІРѕР№ HIPER G16',
+             'description': '16.1", Intel Core i5 10400 2.9Р“Р“С†, 6-СЏРґРµСЂРЅС‹Р№, 16Р“Р‘ DDR4, 512Р“Р‘ SSD, NVIDIA GeForce RTX 3070 РґР»СЏ РЅРѕСѓС‚Р±СѓРєРѕРІ - 8 Р“Р‘, Р±РµР· РѕРїРµСЂР°С†РёРѕРЅРЅРѕР№ СЃРёСЃС‚РµРјС‹, С‡РµСЂРЅС‹Р№ [g16rtx3070a10400lx]',
+             'price': 125990, 'category': Category.objects.get(title='РќРѕСѓС‚Р±СѓРєРё')},
+            {'title': 'РЎРјР°СЂС‚С„РѕРЅ Huawei Mate 50 Pro 8/512Gb', 'description': 'РѕСЂР°РЅР¶РµРІС‹Р№', 'price': 79990,
+             'category': Category.objects.get(title='РЎРјР°СЂС‚С„РѕРЅС‹')},
+            {'title': 'РЎРјР°СЂС‚С„РѕРЅ REALME C25s 4/64Gb', 'description': 'СЃРµСЂС‹Р№', 'price': 8990,
+             'category': Category.objects.get(title='РЎРјР°СЂС‚С„РѕРЅС‹')},
+            {'title': '50" РўРµР»РµРІРёР·РѕСЂ HAIER Smart TV S1', 'description': '4K Ultra HD, С‡РµСЂРЅС‹Р№, РЎРњРђР Рў РўР’, Android',
+             'price': 27990, 'category': Category.objects.get(title='РўРµР»РµРІРёР·РѕСЂС‹')},
+            {'title': '43" РўРµР»РµРІРёР·РѕСЂ Xiaomi Mi TV A2', 'description': '4K Ultra HD, С‡РµСЂРЅС‹Р№, РЎРњРђР Рў РўР’, Android',
+             'price': 24990, 'category': Category.objects.get(title='РўРµР»РµРІРёР·РѕСЂС‹')},
+            {'title': 'РњРѕРЅРёС‚РѕСЂ Huawei MateView SE SSN-24 23.8"', 'description': 'С‡РµСЂРЅС‹Р№ [53060683]', 'price': 11990,
+             'category': Category.objects.get(title='РњРѕРЅРёС‚РѕСЂС‹')},
         ]
 
         products_for_append = []
