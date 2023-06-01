e0004ef48bfaebcd51d91ccfcf56f988c7c69350
Artem <akirillov@itps-russia.ru>
2023-06-01 20:15:42 +0500

    20.1 Реализация базового шаблона и подшаблона. Добавление страницы с категориями товаров

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from catalog.views import index, contacts
+from catalog.apps import CatalogConfig
+from catalog.views import index, contacts, category
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-        path('', index),
-        path('contacts', contacts)
+        path('', index, name='index'),
+        path('contacts/', contacts, name='contacts'),
+        path('category/', category, name='category')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -4,12 +4,25 @@ from catalog.models import Category, Product
 
 
 # Create your views here.
+company_name = 'Магазин на диване'
+
 def index(request):
     product_list = Product.objects.all()
     context = {
-        'product_list': product_list
+        'product_list': product_list,
+        'title': 'Товары',
+        'company_title': company_name
     }
     return render(request, 'catalog/index.html', context)
+
+def category(request):
+    category_list = Category.objects.all()
+    context = {
+        'category_list': category_list,
+        'title': 'Категории',
+        'company_title': company_name
+    }
+    return render(request, 'catalog/category.html', context)
 
 def contacts(request):
     if request.method == 'POST':
