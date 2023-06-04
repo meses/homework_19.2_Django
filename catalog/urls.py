ce248efc2be42fc1596dc674bd0c677d1e6bd1d9
Artem <akirillov@itps-russia.ru>
2023-06-04 17:55:13 +0500

    21.1 Для Product реализован CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import index, contacts, category, product_add
+from catalog.views import contacts, category, ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-        path('', index, name='index'),
+        path('', ProductListView.as_view(), name='index'),
         path('contacts/', contacts, name='contacts'),
         path('category/', category, name='category'),
-        path('product_add/', product_add, name='product_add')
+        path('product_add/', ProductCreateView.as_view(), name='product_add'),
+        path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
+        path('product/update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
+        path('product/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,33 +1,87 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+
 from catalog.models import Category, Product
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 
 # Create your views here.
 company_name = 'Магазин на диване'
 
-def index(request):
-    product_list = Product.objects.all()
-    context = {
-        'product_list': product_list,
+class ProductListView(ListView):
+    model = Product
+    extra_context = {
         'title': 'Товары',
         'company_title': company_name
     }
-    return render(request, 'catalog/index.html', context)
 
-def product_add(request):
-    category_list = Category.objects.all()
-    content = {
-        'category_list': category_list
+class ProductDetailView(DetailView):
+    model = Product
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['title'] = self.get_object().title
+        return context_data
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ('title', 'description', 'price', 'category',)
+    success_url = reverse_lazy('catalog:index')
+    extra_context = {
+        'title': 'Создать товар',
+        'company_title': company_name
     }
-    if request.method == 'POST':
-        title = request.POST.get('title')
-        description = request.POST.get('description')
-        price = request.POST.get('price')
-        category_id = request.POST.get('category_id')
-        Product.objects.create(title=title, description=description, price=price, category_id=category_id)
-        #print(f'{title}, {category_id}')
 
-    return render(request, 'catalog/product_add.html', content)
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ('title', 'description', 'price', 'category',)
+    success_url = reverse_lazy('catalog:index')
+    extra_context = {
+        'title': 'Изменить товар',
+        'company_title': company_name
+    }
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('catalog:index')
+    extra_context = {
+        'title': 'Удалить товар',
+        'company_title': company_name
+    }
+
+#def product(request, pk):
+#    product_item = Product.objects.get(pk=pk)
+#    context = {
+#        'product' : product_item,
+#        'title': 'Товар',
+#        'company_title': company_name
+#    }
+#    return render(request, 'catalog/product_detail.html', context)
+
+#def index(request):
+#    product_list = Product.objects.all()
+#    context = {
+#        'product_list': product_list,
+#        'title': 'Товары',
+#        'company_title': company_name
+#    }
+#    return render(request, 'catalog/product_list.html', context)
+
+
+#def product_add(request):
+#    category_list = Category.objects.all()
+#    content = {
+#        'category_list': category_list
+#    }
+#    if request.method == 'POST':
+#        title = request.POST.get('title')
+#        description = request.POST.get('description')
+#        price = request.POST.get('price')
+#        category_id = request.POST.get('category_id')
+#        Product.objects.create(title=title, description=description, price=price, category_id=category_id)
+#        #print(f'{title}, {category_id}')
+#
+#    return render(request, 'catalog/product_add.html', content)
 
 def category(request):
     category_list = Category.objects.all()
