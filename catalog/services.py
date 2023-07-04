25147e4d3b09ee3244bb4987fb2987b13161ad4b
Artem <akirillov@itps-russia.ru>
2023-07-05 00:52:50 +0500

    23.2 Задания 3 и 4.

--- catalog/services.py
+++ catalog/services.py
@@ -1,6 +1,11 @@
 from django.conf import settings
 from django.core.mail import send_mail
 
+from catalog.models import Category
+from config.settings import CACHE_ENABLED
+from django.core.cache import cache
+
+
 def send_congratuation_email():
     send_mail(
         'Поздравляем!',
@@ -8,3 +13,16 @@ def send_congratuation_email():
         settings.EMAIL_HOST_USER,
         recipient_list=['dvayuzer@yandex.ru']
     )
+
+def get_categories():
+    '''Сервисная функция, которая будет отвечать за выборку категорий + низкоуровневое кэширование'''
+    if CACHE_ENABLED:
+        key = f'categories_list'
+        categories_list = cache.get(key)
+        if categories_list is None:
+            categories_list = Category.objects.all()
+            cache.set(categories_list, categories_list)
+    else:
+        categories_list = Category.objects.all()
+
+    return categories_list
--- config/settings.py
+++ config/settings.py
@@ -11,13 +11,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
-YANDEX_EMAIL = os.environ.get('YANDEX_EMAIL')
-YANDEX_EMAIL_PASSWORD = os.environ.get('YANDEX_EMAIL_PASSWORD')
+#YANDEX_EMAIL = os.environ.get('YANDEX_EMAIL')
+#YANDEX_EMAIL_PASSWORD = os.environ.get('YANDEX_EMAIL_PASSWORD')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -83,8 +85,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django_kirillov_db',
-        'USER': 'postgres',
-        'PASSWORD': 'cnmeses',
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': '127.0.0.1',
         'PORT': '5432'
         #superuser: admin | superparol
@@ -142,8 +144,8 @@ MEDIA_ROOT = BASE_DIR / 'media/'
 
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
-EMAIL_HOST_USER = 'dvayuzer@yandex.ru'
-EMAIL_HOST_PASSWORD = 'gudweasygouitwqk'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 
@@ -152,10 +154,10 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = 'users:login'
 
-CACHE_ENABLED = True
+CACHE_ENABLED = os.getenv('CACHE_ENABLED') == '1'
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379",
+        "LOCATION": os.getenv('CACHE_LOCATION')
     }
 }
